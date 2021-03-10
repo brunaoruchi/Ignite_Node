726c0ffa4305584857deb06a40075539fe1afa84
Bruna Oruchi <brunaquispeo@gmail.com>
2021-03-10 16:25:04 -0300

    Adiciona a validaçao de CPF existente

--- chapter01/primeiro-projeto-com-node-js/src/index.js
+++ chapter01/primeiro-projeto-com-node-js/src/index.js
@@ -16,11 +16,16 @@ const customers = [];
 
 app.post("/account", (request, response) => {
 	const {cpf, name} = request.body;
-	const id = uuid4();
+
+	const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+	if (customerAlreadyExists){
+		return response.status(400).json({ error:"Customer already exists!" });
+	}
+
 	customers.push({
 		cpf,
 		name,
-		id,
+		id: uuid4(),
 		statement: []
 	});
 	return response.status(201).send();
