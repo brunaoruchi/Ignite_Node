f01f24b862f35b454c7c5674afb1314993e87988
Bruna Oruchi <brunaquispeo@gmail.com>
2021-03-18 16:10:30 -0300

    Debugando o codigo

--- chapter02/criando-a-api-com-node-js/src/server.ts
+++ chapter02/criando-a-api-com-node-js/src/server.ts
@@ -1,8 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get("/", (request, response)=>{return response.json({message:"Hello World! o/"})
-})
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World! o/" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is runnig!"));
