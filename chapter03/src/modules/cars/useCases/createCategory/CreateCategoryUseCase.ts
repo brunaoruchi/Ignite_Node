74d54d40d061120d13f930214f47f2739199c3f1
Bruna Oruchi <brunaquispeo@gmail.com>
2021-04-23 19:42:30 -0300

    Adiciona Banco de Dados

---
+++ chapter03/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,30 @@
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * [X] - Definir o tipo de retorno
+ * [X] - Alterar o retorno do erro
+ * [X] - Acessar o repositório
+ * [] - Retornar algo
+ */
+
+class CreateCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  async execute({ description, name }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category Already Exists!");
+    }
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryUseCase };
