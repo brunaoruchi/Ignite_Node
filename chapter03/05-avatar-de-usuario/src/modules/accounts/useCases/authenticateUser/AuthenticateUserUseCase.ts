3f66355f17b3648f47f5e433c333ff4b7f76e0cd
Bruna Oruchi <brunaquispeo@gmail.com>
2021-04-26 15:00:10 -0300

    Atualiza a pasta avatar de usuario

---
+++ chapter03/05-avatar-de-usuario/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+/**
+ * 1 - Usuário existe;
+ * 2 - Senha está correta;
+ * 3 -  Gerar JWT
+ */
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError("Email or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new AppError("Email or password incorrect");
+    }
+
+    const token = sign({}, "d131a9d0e1bab608b88104133c8fc603", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
