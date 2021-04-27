40fe057453aa7cde45f1c1ae521ba1c8a9064a02
Bruna Oruchi <brunaquispeo@gmail.com>
2021-04-27 17:36:57 -0300

    Adiciona o modulo requisitos

---
+++ chapter04/02-Requisitos/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -0,0 +1,42 @@
+import { getRepository, Repository } from "typeorm";
+
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "@modules/cars/repositories/ISpecificationsRepository";
+
+import { Specification } from "../entities/Specification";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private repository: Repository<Specification>;
+  private static INSTANCE: SpecificationsRepository;
+
+  constructor() {
+    this.repository = getRepository(Specification);
+  }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+    return SpecificationsRepository.INSTANCE;
+  }
+
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
+      description,
+      name,
+    });
+
+    await this.repository.save(specification);
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    const specification = await this.repository.findOne({
+      name,
+    });
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
