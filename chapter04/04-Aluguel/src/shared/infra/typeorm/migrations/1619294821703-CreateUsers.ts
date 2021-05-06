419e3a191af55243869e59bd65eb2c2cfa85e7aa
Bruna Oruchi <brunaquispeo@gmail.com>
2021-05-05 23:26:58 -0300

    Adiciona o modulo aluguel

--- chapter04/04-Aluguel/src/shared/infra/typeorm/migrations/1619294821703-CreateUsers.ts
+++ chapter04/04-Aluguel/src/shared/infra/typeorm/migrations/1619294821703-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1619294821703 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
